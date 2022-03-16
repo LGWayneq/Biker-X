0528c6b40bb24c4d1ad597ca6d3bfc4d9a48b831
fudgyfrog <jiekai98@gmail.com>
2022-03-17 03:59:26 +0800

    Added weather icon according to gov API

--- app/src/main/java/com/example/bikerx/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/bikerx/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.bikerx.ui.home;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,22 +12,26 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavDirections;
 import androidx.navigation.Navigation;
 
-import com.example.bikerx.LoginActivity;
-import com.example.bikerx.MainActivity;
+import com.android.volley.VolleyError;
+import com.example.bikerx.IResult;
 import com.example.bikerx.R;
+import com.example.bikerx.VolleyService;
 import com.example.bikerx.databinding.FragmentHomeBinding;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HomeFragment extends Fragment {
     private String TAG = "HOME_FRAGMENT";
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding mBinding;
+    private TextView weatherText;
+    private VolleyService mVolleyService;
+    private IResult mResultCallback;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -35,6 +40,83 @@ public class HomeFragment extends Fragment {
 
         mBinding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = mBinding.getRoot();
+        mResultCallback = new IResult() {
+            @SuppressLint("UseCompatLoadingForDrawables")
+            @Override
+            public void notifySuccess(String requestType, JSONObject response) {
+                if (requestType.equals("GET_WEATHER_DATA")) {
+                    Log.d("HomeFragment", response.toString());
+                    try {
+                        String forecast = response.getJSONArray("items").getJSONObject(0).getJSONArray("forecasts").getJSONObject(12).getString("forecast");
+                        Drawable res;
+                        switch (forecast) {
+                            case "Partly Cloudy (Day)":
+                                res = getResources().getDrawable(getResources().getIdentifier(
+                                        "@drawable/cloudy_day",
+                                        null,
+                                        getActivity().getPackageName()
+                                ));
+                                mBinding.imageWeather.setImageDrawable(res);
+                                break;
+                            case "Partly Cloudy (Night)":
+                                res = getResources().getDrawable(getResources().getIdentifier(
+                                        "@drawable/cloudy_night",
+                                        null,
+                                        getActivity().getPackageName()
+                                ));
+                                mBinding.imageWeather.setImageDrawable(res);
+                                break;
+                            case "Cloudy":
+                                res = getResources().getDrawable(getResources().getIdentifier(
+                                        "@drawable/cloudy",
+                                        null,
+                                        getActivity().getPackageName()
+                                ));
+                                mBinding.imageWeather.setImageDrawable(res);
+                                break;
+                            case "Light Showers":
+                            case "Showers":
+                            case "Moderate Rain":
+                                res = getResources().getDrawable(getResources().getIdentifier(
+                                        "@drawable/rain",
+                                        null,
+                                        getActivity().getPackageName()
+                                ));
+                                mBinding.imageWeather.setImageDrawable(res);
+                                break;
+                            case "Thundery Showers":
+                            case "Heavy Thundery Showers with Gusty Winds":
+                                res = getResources().getDrawable(getResources().getIdentifier(
+                                        "@drawable/storm",
+                                        null,
+                                        getActivity().getPackageName()
+                                ));
+                                mBinding.imageWeather.setImageDrawable(res);
+                                break;
+                            default:
+                                res = getResources().getDrawable(getResources().getIdentifier(
+                                        "@drawable/sun",
+                                        null,
+                                        getActivity().getPackageName()
+                                ));
+                                mBinding.imageWeather.setImageDrawable(res);
+                                break;
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Log.d(TAG, "Volley requester " + requestType);
+                Log.d(TAG, "Volley JSON post" + response);
+            }
+
+            @Override
+            public void notifyError(String requestType, VolleyError error) {
+                Log.d(TAG, "Volley requester " + requestType);
+                Log.d(TAG, "Volley JSON post" + "That didn't work!");
+            }
+        };
+        mVolleyService = new VolleyService(mResultCallback, getContext());
 
         return root;
     }
@@ -42,8 +124,11 @@ public class HomeFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
         bindButtons();
-        displayWeather();
+        getWeatherData();
+//        displayWeather();
+
     }
 
     @Override
@@ -52,8 +137,10 @@ public class HomeFragment extends Fragment {
         mBinding = null;
     }
 
-    private void displayWeather() {
-        //to be implemented
+    private void getWeatherData() {
+        mVolleyService.getDataVolley("GET_WEATHER_DATA", "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast");
+
+
     }
 
     private void bindButtons() {
